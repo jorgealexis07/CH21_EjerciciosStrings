1ad1a8679f089a1eabfa9a0981ae9917f94329e8
jorgealexis07 <201711104@tese.edu.mx>
2022-12-12 17:19:14 -0600

    Agrego expresion regular

--- js/main.js
+++ js/main.js
@@ -27,3 +27,29 @@ function CharAtCadena(cadena) {
         }        
     } */
 }
+
+let btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display="none";
+   /*  console.log(exampleFormControlTextarea1.value);
+    console.log("[" + exampleFormControlTextarea1.value.replaceAll(" "," ")+"]"); */
+    
+    if (exampleFormControlTextarea1.value.trim().length < 20){
+        alertError.innerHTML = "El mensaje debe de contener 20 caracteres o más";
+        alertError.style.display= "block";
+        exampleFormControlTextarea1.focus(); 
+        exampleFormControlTextarea1.select();
+    }//if
+
+    if (exampleFormControlInput1.value.match(emailRegex)== null) {
+        alertError.style.display= "block";
+        alertError.innerHTML +="<br/>El correo electrónico no es valido.";
+    }//
+});
